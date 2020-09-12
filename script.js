ec1e126ad3092a72de11b481aae70afeb21a5656
connietee22 <connietsangphotos@gmail.com>
2020-09-12 10:38:53 -0400

    removed scrolling-behaviour on CSS and added smooth scrolling through jQuery for all a tags

--- script.js
+++ script.js
@@ -190,9 +190,33 @@ dumplingApp.init = function() {
   //     }, 1000);
   //     return;
   // })
+// TO ANIMATE ON SCROLL
+
+
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      
+
+      // Store hash
+      const hash = this.hash;
+      event.preventDefault();
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+
   dumplingApp.validateForm();
   dumplingApp.submitChoices();
 
+})
 };
 
 $(document).ready(function(){
