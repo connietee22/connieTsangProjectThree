f1fafc2adfbbf468acf00f84ca150c7e1b73c470
connietee22 <connietsangphotos@gmail.com>
2020-09-11 21:00:25 -0400

    added functionality to produce results when both fillings selected | moved code into functions

--- script.js
+++ script.js
@@ -78,6 +78,7 @@ const dumplings = {
 }
 
 const dumplingApp = {};
+dumplingApp.countryResults = []; // *** HELPCUE - RESULTS WOULD NOT POST UNLESS I PUT THIS IN GLOBAL SCOPE
 
 // check if at least one box has been checked in each section before proceeding
 dumplingApp.validateForm = function () {
@@ -102,10 +103,10 @@ dumplingApp.submitChoices = function() {
   
   $('.dumplingSelection').on('submit', function(e) {
     e.preventDefault();
-    $('.finalDumping').empty();
     // get the value of selected checkboxes in boiled || fried
     dumplingApp.dumplingType = $('input[type=radio]:checked').val();
-    // get value of meat || veggie or both and put in variable 
+
+    // get value of meat || veggie or both and put in variable
     if ($('input[value=veg]:checked').val() && $('input[value=meat]:checked').val()) {
       dumplingApp.fillingType = "both";
     } else if ($('input[value=meat]:checked').val()) {
@@ -114,63 +115,65 @@ dumplingApp.submitChoices = function() {
       dumplingApp.fillingType = "veg";
     }
 
-  // cycle through dumplings arrays to compare user’s selections in variables to fried and/or boiled filling values in dumpling objects.
-    // push results into array
-    const results = [];
-    for (let i = 0; i < dumplings[dumplingApp.dumplingType].length; i++) {
-      if (dumplings[dumplingApp.dumplingType][i].filling === dumplingApp.fillingType) {
-        results.push(dumplings[dumplingApp.dumplingType][i]);
-      }
-    }
-    // need to find a way to get both in the mix
-
-    // display the country of origin from these results as radio buttons 
-    const displayCountries = function(filteredResults) {
-      $countries.empty();
-      $finalDumpling.empty();
-      $countries.append(`<p>Choose your dumpling's origin</p>
-        <form class="countriesFlex"></form>`);
-      filteredResults.forEach((result) => {
-        $('.countriesFlex').append(`
-          <input type="radio" id="${result.origin}" name="country" value="${result.origin}" aria-hidden="true">
-          <label for="${result.origin}" aria-label="click to display dumpling with origin of ${result.origin}" tabindex="0">${result.origin}</label>
-        `);
+    const chosenDumplingType = dumplings[dumplingApp.dumplingType];
+    console.log(chosenDumplingType);
+    // filling in countryResults array with appropriate dumplings -- both will have everything with a meat and veg filling
+    if (dumplingApp.fillingType === "both") {
+      countryResults = chosenDumplingType;
+    } else {
+    // otherwise, if user selects meat OR veg choice, cycle through dumpling objects to only include appropriate dumpling in new countryResults array  
+      countryResults = chosenDumplingType.filter((dumpling) => {
+        return dumpling.filling === dumplingApp.fillingType;
       })
     }
-    
-    displayCountries(results);
+    // displays countries of all relevant dumplings
+    dumplingApp.displayCountries(countryResults) //****HELPCUE Why does this not work in init? DOES NOT SHOW COUNTRIES*/
+  });
+}  
+
+  //DISPLAYING THE FINAL DUMPLING AS A CHOICE FROM THE COUNTRY BUTTONS
+  dumplingApp.displayCountries = function(filteredResults) {
+    $countries.empty();
+    $finalDumpling.empty();
+    $countries.append(`<p>Choose your dumpling's origin</p>
+      <form class="countriesFlex"></form>`);
+    filteredResults.forEach((result) => {
+      $('.countriesFlex').append(`
+        <input type="radio" id="${result.origin}" name="country" value="${result.origin}" aria-hidden="true">
+        <label for="${result.origin}" aria-label="click to display dumpling with origin of ${result.origin}" tabindex="0">${result.origin}</label>
+      `);
+    });
+    dumplingApp.displayFinal();
+  }
+
     // event listener on new form 
     // when button is clicked, the corresponding dumpling + recipe link will appear
+  dumplingApp.displayFinal = function() {
     $('[name=country]').on('click', function (e) {
       e.preventDefault();
       const countrySelected = $(this).val();
-      console.log(countrySelected);
-      console.log(results);
-      results.forEach(result => {
+      console.log(this);
+      console.log(countryResults);
+      countryResults.forEach(result => {
         if (result.origin === countrySelected) {
           $finalDumpling.empty();
-          $finalDumpling.append(`<p><span class="finalIntro">Get your dumpling recipe:</span> <a href="${result.recipe}">${result.name}</a></p>`).addClass('addBg');
+          $finalDumpling.append(`<p><span class="finalIntro">Get your dumpling recipe:</span> <span class="highlight"><a href="${result.recipe}">${result.name}</a></span></p>`).addClass('addBg');
         }
       })
-      const finalDumpling = results[countrySelected];
-      console.log(finalDumpling);
     });
-
-  })
-
-
-
-}
-
-
-
-
+  };
+// } // BRINGING UP TO CLOSE SUBMIT CHOICES
 
 
 
 dumplingApp.init = function() {
+  // const chosenDumplingType = dumplings[dumplingApp.dumplingType];
   dumplingApp.validateForm();
   dumplingApp.submitChoices();
+  dumplingApp.displayCountries(dumplingApp.countryResults)
+  // dumplingApp.displayFinal(); 
+  
+  
   
 };
 
